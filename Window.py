c6133c6aba33670ea37a352d34ce5b1d122e7d63
Khaled-Mrad <52129183+Khaled-Mrad@users.noreply.github.com>
2019-07-01 23:01:32 +0200

    add a button and interaction

--- Window.py
+++ Window.py
@@ -1,10 +1,31 @@
 from tkinter import *
+import webbrowser
+
+def khaled():
+    webbrowser.open_new("http://youtube.com")
+
 window=Tk()
-window.title("Good Morning")
+window.title("my new app")
 window.geometry("500x500")
 window.minsize(200,200)
-window.config(background="yellow")
-label_title=Label(window,text="Python is WWOOWW",font=("Oxygen",40),
-                  bg="red",fg="blue")
-label_title.pack(expand=YES)
+window.config(background="#00B8FF")
+
+frame=Frame(window,bg="#59E959",bd=1,relief=SUNKEN)
+
+
+label_title=Label(frame,text="Python is WWOOWW",font=("Oxygen",40),
+                  bg="white",fg="blue")
+label_title.pack()
+#new text
+
+label_text=Label(frame,text="my naem  is khaled maf",font=("Oxygen",25),
+                  bg="white",fg="blue")
+label_text.pack()
+
+#new button
+khaled=Button(frame,text="youtube",font=("Oxygen",25),
+                  bg="white",fg="blue",command=khaled)
+khaled.pack(pady=25,fill=X)
+frame.pack(expand=YES)
+
 window.mainloop()
